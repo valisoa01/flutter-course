ccd4c975b8c16e5985d9856964b9a66f242dda33
valisoa01 <valisoatolotriniaina@gmail.com>
2026-08-25 10:18:39 +0300

    feat(dependancy): feature provider

--- lib/main.dart
+++ lib/main.dart
@@ -6,6 +6,8 @@ import 'package:provider/provider.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<CountBloc>(
--- lib/pages/counterPage.dart
+++ lib/pages/counterPage.dart
@@ -3,11 +3,13 @@ import 'package:fluttercourse/blocs/count_bloc.dart';
 import 'package:provider/provider.dart';
 
 class CounterPage extends StatelessWidget{
+  const CounterPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     final CountBloc countBloc = Provider.of<CountBloc>(context);
      return Scaffold(
-      body: new Container(
+      body: Container(
         child: Center(
           child: Text(countBloc.counter.toString()),
         ),
